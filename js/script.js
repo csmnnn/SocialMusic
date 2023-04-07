c58bc47dca25e40d4c25507380817f3905619401
Cosmin-Iulian Manole <cosmin-iulian.manole@student.tuiasi.ro>
2023-04-07 16:32:17 +0300

    SM-8 Updated Profile UI.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-var musicPlayer = document.getElementById('music-player')
-var progress = document.getElementById('progress')
-var progress_slider = document.getElementById('progress-slider')
-var playSpan = document.getElementById('spanPlay')
-const circle = document.getElementById('circle')
+var musicPlayer = document.getElementById('music-player');
+var progress = document.getElementById('progress');
+var progress_slider = document.getElementById('progress-slider');
+var playSpan = document.getElementById('spanPlay');
+const circle = document.getElementById('circle');
 
 musicPlayer.ontimeupdate = function (event) {
-    var coef = (100 * musicPlayer.currentTime) / musicPlayer.duration
-    console.log(event)
-    progress.style.width = Math.floor(coef) + "%"
+    var coef = (100 * musicPlayer.currentTime) / musicPlayer.duration;
+    console.log(event);
+    progress.style.width = Math.floor(coef) + "%";
 }
 progress_slider.onclick = function (event) {
-    musicPlayer.currentTime = (event.offsetX / progress_slider.offsetWidth) * musicPlayer.duration
+    musicPlayer.currentTime = (event.offsetX / progress_slider.offsetWidth) * musicPlayer.duration;
 }
 
 playSpan.onclick = function (event) {
-    event.target.paused = true
-    console.log(event.target.paused)
-    console.log('Play song')
+    event.target.paused = true;
+    console.log(event.target.paused);
+    console.log('Play song');
 }
 
-var likeButton = document.getElementById('like')
+var likeButton = document.getElementById('like');
 
 if (likeButton != null) {
     document.getElementById('like').onclick = function() {
         if (document.getElementById('like').src == "http://127.0.0.1:5500/resources/img/heart_empty.png") {
-            document.getElementById('like').src = "http://127.0.0.1:5500/resources/img/heart_full.png"
+            document.getElementById('like').src = "http://127.0.0.1:5500/resources/img/heart_full.png";
         } else {
-            document.getElementById('like').src = "http://127.0.0.1:5500/resources/img/heart_empty.png"
+            document.getElementById('like').src = "http://127.0.0.1:5500/resources/img/heart_empty.png";
         }
     }
 }
@@ -41,12 +41,12 @@ const openEditArtistsModal = document.getElementById('btnEditArtists');
 const openEditSongsModal = document.getElementById('btnEditSongs');
 const openEditAlbumsModal = document.getElementById('btnEditAlbums');
 
-const songs = document.getElementsByClassName('songs');
-if (songs) {
-    for (let song of songs) {
-        song.addEventListener('click', () => {
+const favSongs = document.getElementsByClassName('fav-img-song');
+if (favSongs) {
+    for (let favSong of favSongs) {
+        favSong.addEventListener('click', () => {
             modalContainer.classList.add('show');
-        })
+        });
     }
 }
 
@@ -65,15 +65,15 @@ if (openEditProfileModal && openEditArtistsModal && openEditSongsModal && openEd
     
     openEditArtistsModal.addEventListener('click', () => {
         modalContainer.classList.add('show');
-    })
+    });
     
     openEditSongsModal.addEventListener('click', () => {
         modalContainer.classList.add('show');
-    })
+    });
     
     openEditAlbumsModal.addEventListener('click', () => {
         modalContainer.classList.add('show');
-    })
+    });
 }
 
 closeModal.addEventListener('click', () => {
